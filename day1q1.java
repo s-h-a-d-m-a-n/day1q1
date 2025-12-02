eba94fceb08e73ec8bcaf2da168390c840534b4c
shadman <mohammadshadman66@gmail.com>
2025-12-03 00:27:15 +0530

    Update day1q1.java

--- day1q1.java
+++ day1q1.java
@@ -1,20 +1,21 @@
-//02.12.2025 by shadman...
+//02.12.2025 by shadman....
 import java.util.*;
 import java.io.File;
 public class day1q1
 {   public static void main(String args[])throws Exception
     {
         Scanner in=new Scanner(new File("doc.txt"));
-        int pass = 0, x, y, sum = 50; String s;                           //x = direction (l=-, r=+)...y = value....
+        int pass = 0, x, y, sum = 50, mult=1; String s;                           //x = direction (l=-, r=+)...y = value....
         while(in.hasNextLine())
         {   s = in.nextLine();
             s.trim();
             x = s.charAt(0) == 'L' ? -1 : +1;
             s = s.substring(1);
             y = Integer.parseInt(s);
+            mult = (y<100)? 1: y%100 ;
             x = x * y;
             sum = sum + x;
-            sum = (sum < 0)? (sum+100) : (sum > 99)? (sum-100) : sum;
+            sum = (sum < 0)? (sum+(mult*100)) : (sum > 99)? (sum-(mult*100)) : sum;
             pass = (sum == 0)? (pass+1) : pass; 
         }
         System.out.println(pass);
