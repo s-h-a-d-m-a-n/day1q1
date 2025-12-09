138ea2be48b7d475f57fafbd3c1977083aabc8ef
shadman <mohammadshadman66@gmail.com>
2025-12-09 23:33:19 +0530

    Update day1q1.java

--- day1q1.java
+++ day1q1.java
@@ -1,24 +1,22 @@
-//02.12.2025 by shadman....
 import java.util.*;
 import java.io.File;
 public class day1q1
 {   public static void main(String args[])throws Exception
     {
-        Scanner in=new Scanner(new File("doc.txt"));
-        int pass = 0, x, y, sum = 50, mult=1; String s;                           //x = direction (l=-, r=+)...y = value....
+        Scanner in=new Scanner(new File("ans.txt"));
+        int password = 0, rotation, value, sum = 50; String subString;                          
         while(in.hasNextLine())
-        {   s = in.nextLine();
-            s.trim();
-            x = s.charAt(0) == 'L' ? -1 : +1;
-            s = s.substring(1);
-            y = Integer.parseInt(s);
-            mult = (y<100)? 1: y%100 ;
-            x = x * y;
-            sum = sum + x;
-            sum = (sum < 0)? (sum+(mult*100)) : (sum > 99)? (sum-(mult*100)) : sum;
-            pass = (sum == 0)? (pass+1) : pass; 
+        {   subString = in.nextLine();
+            subString.trim();
+            rotation = subString.charAt(0) == 'L' ? -1 : +1;
+            subString = subString.substring(1);
+            value = Integer.parseInt(subString);
+            rotation = rotation * value;
+            sum = (sum + rotation) % 100;
+            if (sum < 0) sum += 100;        
+            if (sum == 0) password++; 
         }
-        System.out.println(pass);
+        System.out.println(password);
         in.close();
     }
 }
